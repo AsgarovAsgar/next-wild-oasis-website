e811fb002ad9d022956d7d470784da0dd46f0654
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-06-07 21:50:27 +0400

    create layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,22 @@
+import Logo from './components/Logo'
+import Navigation from './components/Navigation'
+
+export const metadata = {
+  title: 'Wild Oasis',
+  description: 'Wild Oasis'
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright 2025 - All rights reserved</footer>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Navigation from './components/Navigation'
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
+      <h1>Home Page</h1>
     </div>
   )
 }
