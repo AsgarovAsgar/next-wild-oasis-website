9acef5f75931175869ca460699904cbcd9a0a433
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-06-09 15:04:12 +0400

    improve navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import Logo from '@/app/_components/Logo'
-import Navigation from '@/app/_components/Navigation'
+import Header from '@/app/_components/Header'
 import '@/app/_styles/globals.css'
 
 import { Josefin_Sans } from 'next/font/google'
@@ -20,13 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`min-h-screen bg-primary-950 text-primary-100 ${josefinSans.className}`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright 2025 - All rights reserved</footer>
+      <body
+        className={`flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased ${josefinSans.className}`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   )
