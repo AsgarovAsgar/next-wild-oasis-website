906a317cff100642cf56eb3ca94e235e6b0a8ceb
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-06-12 11:17:39 +0400

    optimize logo

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import logo from '@/public/logo.png'
 
 export default function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
-      <span className="text-primary-100 text-xl font-semibold">The Wild Oasis</span>
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      <Image src={logo} className="size-14" alt="The Wild Oasis logo" />
+      <span className="text-xl font-semibold text-primary-100">The Wild Oasis</span>
     </Link>
   )
 }
