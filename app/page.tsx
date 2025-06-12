4f25fd132990be6179b79dbecf8c18a83ca53968
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-06-12 11:26:57 +0400

    build home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
+import bg from '@/public/bg.png'
+
 export default function Page() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        className="object-cover object-top"
+        placeholder="blur"
+        quality={80}
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="mb-10 text-8xl font-normal tracking-tight text-primary-50">Welcome to paradise.</h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-lg font-semibold text-primary-800 transition-all hover:bg-accent-600"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
   )
 }
