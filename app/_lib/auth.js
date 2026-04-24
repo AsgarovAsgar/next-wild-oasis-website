66d0976b5585cb150318171ce4f79cfb3b440865
Asgar Asgarov <asgarovasgar28@gmail.com>
2026-04-24 20:08:22 +0400

    create a new guest on the first sign

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { getGuest, createGuest } from '@/app/_lib/data-service'
 
 export const options = {
   providers: [
@@ -11,6 +12,26 @@ export const options = {
   callbacks: {
     authorized({ auth }) {
       if (!!auth?.user) return true
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email)
+        if (!existingGuest) {
+          await createGuest({
+            email: user.email,
+            fullName: user.name
+          })
+        }
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+    async session({ session }) {
+      const guest = await getGuest(session.user.email)
+      session.user.guestId = guest.id
+      return session
     }
   },
   pages: {
