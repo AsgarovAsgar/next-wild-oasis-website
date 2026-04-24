ec64824d094cb60dece7ae9d8c7dd565be41649f
Asgar Asgarov <asgarovasgar28@gmail.com>
2026-04-24 18:31:44 +0400

    get user session

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -1,14 +1,17 @@
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service'
 import ReservationForm from '@/app/_components/ReservationForm'
 import DateSelector from '@/app/_components/DateSelector'
+import LoginMessage from '@/app/_components/LoginMessage'
+import { auth } from '@/app/_lib/auth'
 
 export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)])
+  const session = await auth()
 
   return (
     <div className="grid min-h-[400px] grid-cols-2 border border-primary-800">
       <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
     </div>
   )
 }
