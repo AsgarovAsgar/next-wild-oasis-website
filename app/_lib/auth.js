0c3e3a540fee89b0756f10ea8fcee63194a9a550
Asgar Asgarov <asgarovasgar28@gmail.com>
2026-04-24 18:51:29 +0400

    protect routes with middleware

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -7,7 +7,12 @@ export const options = {
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    authorized({ auth }) {
+      if (!!auth?.user) return true
+    }
+  }
 }
 
 export const {
