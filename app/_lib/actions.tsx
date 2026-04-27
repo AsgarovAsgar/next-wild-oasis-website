aeec833442b39f3eeaffddefaba407580f275ad7
Asgar Asgarov <asgarovasgar28@gmail.com>
2026-04-27 17:16:13 +0400

    add delete reservation

--- app/_lib/actions.tsx
+++ app/_lib/actions.tsx
@@ -4,6 +4,7 @@ import { signIn, signOut } from '@/app/_lib/auth'
 import { auth } from './auth'
 import { supabase } from './supabase'
 import { revalidatePath } from 'next/cache'
+import { getBookings } from './data-service'
 
 export async function updateGuest(formData: FormData) {
   const session = await auth()
@@ -26,6 +27,21 @@ export async function updateGuest(formData: FormData) {
   revalidatePath('/account/profile')
 }
 
+export async function deleteReservation(bookingId) {
+  const session = await auth()
+  if (!session) throw new Error('You must be logged in')
+
+  const guestBookings = await getBookings(session?.user?.guestId)
+  const guestBookingIds = guestBookings.map((booking) => booking.id)
+  if (!guestBookingIds.includes(bookingId)) throw new Error('You are not allowed to delete this reservation')
+  console.log('guestBookingIds', guestBookingIds)
+  console.log('deleting', bookingId)
+
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId)
+  console.log('err', error)
+  if (error) throw new Error('Booking could not be deleted')
+  revalidatePath('/account/reservations')
+}
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' })
 }
